72bd965cf24b105e4d3b941bb9dc0691645f2822
三咲智子 <sxzz@sxzz.moe>
2022-04-14 04:50:12 +0800

    test: add esbuild target

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vitest/config'
+import esbuild from 'rollup-plugin-esbuild'
 
 export default defineConfig({
   plugins: [
+    esbuild({ target: 'node14' }),
     {
       name: 'FetchPloyfill',
       transform(code, id) {
