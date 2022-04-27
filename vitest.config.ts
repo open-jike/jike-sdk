abe415d55129f64a2146362f087f416f7bcbac0c
三咲智子 <sxzz@sxzz.moe>
2022-04-28 02:25:58 +0800

    test: add node 18

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,22 @@
 import { defineConfig } from 'vitest/config'
 import esbuild from 'rollup-plugin-esbuild'
+import compareVersions from 'compare-versions'
+import type { Plugin } from 'rollup'
+
+const FetchPolyfill: Plugin = {
+  name: 'FetchPloyfill',
+  transform(code, id) {
+    if (!id.endsWith('src/request.ts')) return
+    return `import './node-shim'; ${code}`
+  },
+}
+
+const plugins: Plugin[] = [esbuild({ target: 'node14' })]
+
+// Enable ployfill when node < 18
+if (compareVersions.compare(process.version, '18', '<'))
+  plugins.push(FetchPolyfill)
 
 export default defineConfig({
-  plugins: [
-    esbuild({ target: 'node14' }),
-    {
-      name: 'FetchPloyfill',
-      transform(code, id) {
-        if (!id.endsWith('src/request.ts')) return
-        return `import './node-shim'; ${code}`
-      },
-    },
-  ],
+  plugins,
 })
