f119e230e9d2d808a0e8c35a63129c74eac9ae6e
river <laolei@forkway.cn>
2024-02-10 16:24:51 +0800

    feat(notifications): add merged_comment notification test and types

--- src/types/entity/notification.ts
+++ src/types/entity/notification.ts
@@ -1,6 +1,7 @@
 import type { LiteralUnion } from '../../utils'
 import type { Picture, PostStatus, PostType } from './post'
 import type { User } from './user'
+import {Post} from './post';
 
 export interface Notification {
   id: string
@@ -20,6 +21,8 @@ export interface Notification {
     | 'PERSONAL_UPDATE_REPOSTED'
     | 'LIKE_AVATAR'
     | 'AVATAR_GREET'
+    | 'MERGED_COMMENT'
+    | 'MENTION_FROM_UNFOLLOWED_USER'
   >
   /**
    * ISO-8601 格式，如 `2015-03-04T00:00:00.000Z`
@@ -35,7 +38,7 @@ export interface Notification {
   actionType: LiteralUnion<'actionType' | 'USER_LIST'>
   actionItem: ActionItem
   linkUrl: string
-  linkType: LiteralUnion<PostType>
+  linkType: LiteralUnion<PostType> | 'MERGED_MENTION'
   referenceItem: ReferenceItem
 }
 
@@ -64,3 +67,8 @@ export interface ReferenceItem {
   /** 引用图片地址 */
   referenceImageUrl?: string
 }
+
+export type NotificationDetailPost = {
+  id : string
+
+} & Post
