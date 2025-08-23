97d7fd369a29f30581c775c03c37b847cfb7f796
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-08-23 09:56:57 +0800

    feat!: drop node 18

--- src/utils/uuid.ts
+++ src/utils/uuid.ts
@@ -1,16 +1,3 @@
-import type { webcrypto } from 'node:crypto'
-let _crypto: any
-import('node:crypto')
-  .then((c) => (_crypto = c))
-
-  .catch(() => null)
-
 export function generateUUID(): string | undefined {
-  try {
-    const crypto: webcrypto.Crypto | undefined =
-      (globalThis as any).crypto || _crypto
-    if (!crypto) return undefined
-    return crypto.randomUUID()
-  } catch {}
-  return undefined
+  return crypto.randomUUID()
 }
